a745ab0ce98f92961b1cf5e9ec502029db51db3f
Sh3llcod3 <4896814-Sh3llcod3@users.noreply.gitlab.com>
2020-02-04 23:59:06 +0000

    Prepping changes.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,1 +1,14 @@
 # Contribution Guidelines
+
+Start by installing pyenv and poetry.
+
+Then clone the repo and change into the relevant directory.
+
+Create the poetry virtualenv with `poetry install`
+
+Run the virtualenv: `poetry shell`
+
+From there, to deploy this app, just type `ingest_server`
+
+Feel free to suggest code changes.
+All code is formatted and tested to be compliant with `flake8`.
--- code_ingest/__main__.py
+++ code_ingest/__main__.py
@@ -2,7 +2,12 @@ from os import environ
 
 import uvicorn
 
-IFACE = environ.get("INGEST_SERVER_HOST", "0.0.0.0")  # noqa: S104
-PORT = int(environ.get("INGEST_SERVER_PORT", "5050"))
+def main():
 
-uvicorn.run("code_ingest.ingest_server:app", host=IFACE, port=PORT, debug=False)
+    IFACE = environ.get("INGEST_SERVER_HOST", "0.0.0.0")  # noqa: S104
+    PORT = int(environ.get("INGEST_SERVER_PORT", "5050"))
+
+    uvicorn.run("code_ingest.ingest_server:app", host=IFACE, port=PORT, debug=False)
+
+if __name__ == "__main__":
+    main()
