b78b31fa7c32c5e5002962d00d1915887e85c987
Sh3llcod3 <4896814-Sh3llcod3@users.noreply.gitlab.com>
2020-02-05 00:01:36 +0000

    Small changes

--- code_ingest/__main__.py
+++ code_ingest/__main__.py
@@ -2,12 +2,14 @@ from os import environ
 
 import uvicorn
 
+
 def main():
 
     IFACE = environ.get("INGEST_SERVER_HOST", "0.0.0.0")  # noqa: S104
     PORT = int(environ.get("INGEST_SERVER_PORT", "5050"))
 
     uvicorn.run("code_ingest.ingest_server:app", host=IFACE, port=PORT, debug=False)
+
 
 if __name__ == "__main__":
     main()
